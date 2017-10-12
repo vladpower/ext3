f915f6ce968b794b475ba6b2258b4ac7bf5762a9
Vlad <vlad165432@gmail.com>
2017-10-12 20:00:01 +0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "utils/filemapping.h"
+using namespace std;
+int main(int argc, char* argv[]) {
+    //input path to drive image file and offset from the beginning of the disk in sectors
+    if(argc<2) {
+        cerr<<"too few arguments"
+    }
+
+    return 0;
+}
